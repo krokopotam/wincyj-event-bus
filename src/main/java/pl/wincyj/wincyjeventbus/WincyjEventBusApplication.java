6661658737c7e5bc58c504bb182e6e094b9ce85d
pawel.dajek <pawel.dajek@edge1s.com>
2025-04-21 13:46:17 +0200

    init

---
+++ src/main/java/pl/wincyj/wincyjeventbus/WincyjEventBusApplication.java
@@ -0,0 +1,13 @@
+package pl.wincyj.wincyjeventbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WincyjEventBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WincyjEventBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wincyj/wincyjeventbus/WincyjEventBusApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wincyj.wincyjeventbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WincyjEventBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
